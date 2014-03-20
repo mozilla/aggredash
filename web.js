044b395626e36a4bb105804d17f25697600baed8
Adam Lofting <contactme@adamlofting.com>
2014-03-20 15:12:16 +0000

    CORS

--- web.js
+++ web.js
@@ -13,7 +13,7 @@ app.get('/api', function(req, res) {
   res.send("You're probably looking for /api/mofo/2014");
 });
 
-app.all('/', function(req, res, next) {
+app.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
